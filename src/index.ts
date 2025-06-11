c2381e03e746283150ab60cc336b668bdb6f2bce
freddidit <freddie230569@gmail.com>
2025-06-11 17:47:13 +0300

    Connect & Client Handshake

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,54 @@
+import { connect as connectTls, TLSSocket } from 'tls'
+import { connect as connectNet, Socket } from 'net'
+import { randomBytes } from 'crypto'
+
 export type Credentials = {
     path: string,
-    port: string,
+    port: number,
     secure: boolean,
     host: string
 }
 
-export function parseUrl(urlString: string): Credentials | undefined {
+export function parseUrl(urlString: string) {
     const url = URL.parse(urlString)
-
+    
     if (!url) return
-
+    
     const credentials = {} as Credentials
     const protocol = url.protocol
-
+    
     if (protocol === 'wss:') credentials.secure = true
     else if (protocol === 'ws:') credentials.secure = false
     else throw new Error(`Invalid protocol ${protocol} (ws: | wss:)`)
-
-    credentials.port = url.port || (credentials.secure ? '443' : '80')
+    
+    credentials.port = parseInt(url.port) || (credentials.secure ? 443 : 80)
     credentials.path = url.pathname || '/'
     credentials.host = url.hostname
-
+    
     return credentials
+}
+
+export function clientHandshake(socket: TLSSocket | Socket, credentials: Credentials) {
+    const key = randomBytes(16).toString('base64')    
+    const request = [
+        `GET ${credentials.path} HTTP/1.1`,
+        `Host: ${credentials.host}:${credentials.port}`,
+        `Upgrade: websocket`,
+        `Connection: Upgrade`,
+        `Sec-WebSocket-Key: ${key}`,
+        `Sec-WebSocket-Version: 13`,
+        '', 
+        ''
+  ].join('\r\n')
+
+  socket.write(request)
+}
+
+export function connect(urlString: string) {
+    const credentials = parseUrl(urlString)    
+    if (!credentials) throw new Error('Failed to parse URL')
+    if (credentials.secure) 
+        return connectTls(credentials)
+    else 
+        return connectNet(credentials)        
 }
