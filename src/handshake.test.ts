99d22c914f2604b77e242a54cf15a737a7ccf54a
freddidit <freddie230569@gmail.com>
2025-06-14 21:12:47 +0300

    Simplify test

--- src/handshake.test.ts
+++ src/handshake.test.ts
@@ -3,18 +3,9 @@ import { connect } from './socket'
 import { describe, it } from 'vitest'
 
 describe('WebSocket client handshake', () => {
-  it('does not throw for echo server', async () => {
-    const host = 'echo.websocket.events'
-    const url = new URL('wss://' + host + '/')
-    const socket = connect(url)
-    let error
-    try {
-      await client(socket, url)
-    } catch (err) {
-      error = err
-    } finally {
-      if (socket.destroy) socket.destroy()
-    }
-    if (error) throw error
+  it('Connects to an echo websocket server', async () => {    
+    const url = new URL('wss://' + 'echo.websocket.events' + '/')
+    const socket = connect(url)    
+    await client(socket, url)    
   })
 })
