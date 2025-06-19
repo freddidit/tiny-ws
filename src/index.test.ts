fc83e262cce15bb3f05306dc1c2f426af5ebcb08
freddidit <freddie230569@gmail.com>
2025-06-20 01:44:37 +0300

    Removed the invalid URL test, and adjusted for index.ts changes

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,23 +1,31 @@
 import { describe, it } from 'vitest'
-import { connectWS } from './index'
+import { connectWS, Events } from './index'
+
+const ECHO_SERVER = 'wss://echo.websocket.org'
 
 describe(connectWS, () => {
-    it('Connects to a valid URL', async () => {
-        const url = new URL('wss://echo.websocket.org')
-        const ws = connectWS(url)
+    it('Connects to a valid URL and pings 5 times', async () => {
+        const url = new URL(ECHO_SERVER)
+        const ws = connectWS(url, 1000)
     
         await new Promise<void>((resolve, reject) => {
-            ws.on('open', () => {                
-                resolve()
+            let counter = 1
+            
+            ws.on(Events.OPEN, () => {
+                console.log('Websocket open')
             })
-            ws.on('error', reject)
-        })
-    })
 
-    it('Fails on an invalid URL', async () => {
-        const url = new URL('loreliushogglefart://echo.websocket.org')
-        await new Promise<void>((resolve, reject) => {
-            try {connectWS(url)} catch {resolve()} finally {reject()}
+            ws.on(Events.PONG, rtt => {
+                if (counter >= 5) {
+                    ws.destroy()
+                    resolve()
+                }
+                
+                console.log(`Ping ${counter} | Latency: ${rtt}ms`)
+                counter++
+            })
+
+            ws.on(Events.ERROR, reject)
         })
-    })
+    }, 10000)
 })
